fb4e0586f8bdb7df4cd294530e01e945393e58e7
Voladsky <krylov@sfedu.ru>
2025-09-12 21:29:37 +0300

    feat(miniMaple.test): first TDD tests for miniMaple

--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,5 +1,16 @@
 import {MiniMaple} from "../src/miniMaple";
 
-test('it fails', () => {
-    expect(false).toBeTruthy();
+describe('MiniMaple', () => {
+    it('should differentiate simple polynomials', () => {
+        const miniMaple = new MiniMaple();
+        expect(miniMaple.differentiate("x^2 + 2*x + 1").toString()).toBe("2*x + 2");
+    })
+    it('should be able to differentiate factor with coefficients', () => {
+        const miniMaple = new MiniMaple();
+        expect(miniMaple.differentiate("5*x^6").toString()).toBe("30*x^5");
+    });
+    it('should throw error at ill-formed text', () => {
+        const miniMaple = new MiniMaple();
+        expect(miniMaple.differentiate("x^2 +")).toThrow(MiniMapleError);
+    });
 });
